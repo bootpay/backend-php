182a7afc3e099f03e35b2deea59072d59cad1e88
gosomi <gosomi@udid.co.kr>
2022-07-28 16:01:41 +0900

    실서버로 변경

--- tests/CashCancelOnReceipt.php
+++ tests/CashCancelOnReceipt.php
@@ -8,16 +8,16 @@ require_once '../vendor/autoload.php';
 
 use Bootpay\ServerPhp\BootpayApi;
 
-//BootpayApi::setConfiguration(
-//    '5b8f6a4d396fa665fdc2b5ea',
-//    'rm6EYECr6aroQVG2ntW0A6LpWnkTgP4uQ3H18sDDUYw='
-//);
-
 BootpayApi::setConfiguration(
-    '59bfc738e13f337dbd6ca48a',
-    'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
-    'development'
+    '5b8f6a4d396fa665fdc2b5ea',
+    'rm6EYECr6aroQVG2ntW0A6LpWnkTgP4uQ3H18sDDUYw='
 );
+
+//BootpayApi::setConfiguration(
+//    '59bfc738e13f337dbd6ca48a',
+//    'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
+//    'development'
+//);
 
 $token = BootpayApi::getAccessToken();
 // var_dump($token);
